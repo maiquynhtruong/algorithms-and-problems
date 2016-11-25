223ed8a9792e1df77bb6bb9c64103ff1b4bb8a31
Mai Truong <maitruong2801@gmail.com>
2016-11-25 13:15:42 -0800

    Added Review Scheduling problem

---
+++ review_scheduling.java
@@ -0,0 +1,13 @@
+
+// Problem: A student has M days to review N subjects. In his experience, reivewing subject j in i days will get him a score of A[i][j].
+// Assuming A[i][j] >= 0 and A[i][j] < A[i+1][j], find X[j] (review days for subject j, with j = 1, 2,... m) such that sum(X[j]) = M
+// and the student achieves the highest score (or sum(A[x[j]][i]) is maximum).
+// Input file: First line: 2 positive integer M and N (0 <= N <= 50, 0 < N <= M <= 100)
+// Next M lines, each line has N number indicating score the student will get if he review subject j in i days
+// Output file: First line: Maximum score student can get
+// Next N lines, each line has 2 number for the index of the subject and the number of days needed to review the subject
+
+
+public class Scheduling {
+
+}
