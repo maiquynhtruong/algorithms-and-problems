ac0836cdcba0c1965ccf161a46b500a0523e0697
Mai Truong <maitruong2801@gmail.com>
2016-11-26 10:19:27 -0800

    Added remove loop method

--- detect_and_remove_loops_linked_list.java
+++ detect_and_remove_loops_linked_list.java
@@ -17,11 +17,43 @@ class Ideone {
 		while (slow != null && fast != null && fast.next != null) {
 			fast = fast.next.next;
 			slow = slow.next;
-			if (slow == fast)
+			if (slow == fast) {
+				removeLoops(head, slow);
+				System.out.println(slow.value);
 				return true;
+				}
+			}
 		}
 		return false;
 	}
+
+	
+	// The proof with very beautiful explanation can be found here: http://javabypatel.blogspot.in/2015/12/detect-loop-in-linked-list.html
+	
+	void removeLoops(Node head, Node cur) {
+		
+		// we know the distance from the beginning of the list to start of loop is the same as distance
+		// from meeting point to start of loop, so we can have one pointer at the beginning, keep the other
+		// at the meeting point and move them in step of 1
+
+		// That's to find out where is the start of the loop. To remove it, we need to keep pointer 2 running in the loop 
+		// for every node pointer 1 moves to. When pointer 2's next reaches pointer 1, we know pointer 2 is now the previous node
+		// before the start of loop node, and we can set pointer 2 to null
+		
+		p1 = head;
+		while (true) {
+			p2 = cur; // point two pointers just met
+			while (p2.next != cur && p2.next != p1)
+				p2 = p2.next;
+			if (p2.next == p1) 
+				break; // we found the start of loop, break out
+			// otherwise we keep pointer 1 moving and start a new loop for pointer 2
+			p1 = p1.next;
+		}
+		// since pointer 2 is the pointer before the start of loop, we can set it to null
+		p2.next = null;
+	}
+
 	public static void main(String[] args) {
 		head = new Node(50);
 		head.next = new Node(20);
@@ -30,7 +62,7 @@ class Ideone {
 		head.next.next.next.next = new Node(10);
 		// Create a loop right here right now
 		head.next.next.next.next = head.next.next;
-		// Visually it looks like so:
+		// Visually it looks like this:
 		// 50 -> 20 -> 15 -> 4
 		//              |    |
 		//               <- 10
