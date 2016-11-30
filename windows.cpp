533f4937cbfdb0b931bb1573f3168106be1b0cfb
Mai Truong <maitruong2801@gmail.com>
2016-11-29 20:29:24 -0800

    Add windows problem

---
+++ windows.cpp
@@ -0,0 +1,14 @@
+// Basically the same problem as two rectangles overlapping but now count the number of windows (rectangles) that needs to be
+// close in order to close the bottommost window. The close button is at the top right corner
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	int u[n], v[n], x[n], y[n];
+	for (int i = 0; i < n; i++) 
+		cin >> u[i] >> v[i] >> x[i] >> y[i];
+
+}
