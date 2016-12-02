0c8d9db52cb2c1bc6d64058562565da3e1d1ed11
Mai Truong <maitruong2801@gmail.com>
2016-12-01 17:32:58 -0800

    not finished

--- windows.cpp
+++ windows.cpp
@@ -3,12 +3,35 @@
 
 #include <iostream>
 using namespace std;
+vector<int> v;
+int count = 0;
 
+bool overlap(int i, int j) {
+	if ((x[i] >= x[j] && x[i] <= u[j]) || (v[i] >= y[j] && v[i] <= v[j]))
+		return true;
+	else 
+		return false;
+}
+
+void dfs(int i) {
+	count++;
+	vis[i] = true;
+	v.push_back(i);
+	for (int j = i+1; j < n; j++) 
+		if (!vis[j] && a[i][j])			
+			dfs(j);
+}
 int main() {
 	int n;
 	cin >> n;
 	int u[n], v[n], x[n], y[n];
+	
 	for (int i = 0; i < n; i++) 
 		cin >> u[i] >> v[i] >> x[i] >> y[i];
-
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++)
+			if (overlap(i, j)) a[i][j] == true;
+		
+	for (vector<int>::iterator it = v.begin(); it != v.end(); ++it)
+		cout << *it << " ";
 }
