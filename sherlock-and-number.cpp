2825d16601fc683913e0ee26a562d074334556d8
Mai Truong <maitruong2801@gmail.com>
2016-12-13 23:49:07 -0500

    A more elegantly short algorithm <3

--- sherlock-and-number.cpp
+++ sherlock-and-number.cpp
@@ -9,24 +9,14 @@ int main() {
 	while (T--) {
 		int N, k, p;
 		cin >> N >> k >> p;
-		A[0] = 0;
-		for (int i = 1; i <= k; i++) {
-			cin >> A[i];
-			// cout << A[i] << " ";
+		for (int i = 0; i < k; i++) {
+			int m = 0; cin >> m;
+			if (m= < p) p++;
 		}
-		// cout << endl;
-		sort(A, A + k + 1);
-		// for (int i = 1; i <= k; i++) cout << A[i] << " "; cout << endl;
+		if (p <= N) cout << p << endl;
+		else cout << -1 << endl;
 		
-		int last = 0, sum = A[1] - A[0] - 1, i =10;
-		while (sum < p && sum > 0) {
-			i++;
-			sum += A[i] - A[i-1] - 1;
-			cout << i << ", sum= " << sum << endl;
-		}
 		
-		sum -= A[i] - A[i-1] - 1;
-		cout << A[i] + p - sum << endl;
 	}
 	return 0;
 }
