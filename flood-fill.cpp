fff0414ae1c1f1b6ed42ee44c042554fed345b0e
Mai Truong <maitruong2801@gmail.com>
2016-12-17 16:30:07 -0500

    Update flood-fill.cpp

--- flood-fill.cpp
+++ flood-fill.cpp
@@ -1,1 +1,15 @@
-// So I don't know the name in English but can translate the discription
+# Flood fill 8 connected polygon. Boundary color must be different from fill color
+# so that we know when to stop
+# Initialize seed point, fcol and dcol
+def floodfill(i, j, fcol, dcol):
+	if getPixel(i, j) != dcol and getPixel(i, j) != fcol:
+		setPixel(i, j, fcol)
+		floodfill(i, j-1, fcol, dcol) # fcol: fill color, dcol: default color
+		floodfill(i, j+1, fcol, dcol)
+		floodfill(i-1, j-1, fcol, dcol)
+		floodfill(i-1, j, fcol, dcol)
+		floodfill(i-1, j+1, fcol, dcol)
+		floodfill(i+1, j-1, fcol, dcol)
+		floodfill(i+1, j, fcol, dcol)
+		floodfill(i+1, j+1, fcol, dcol)
+
