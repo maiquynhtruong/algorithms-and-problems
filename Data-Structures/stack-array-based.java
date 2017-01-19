45064cd8d14d6243f9f496c4452fa909d3a4e96c
Mai Truong <maitruong2801@gmail.com>
2017-01-19 17:08:44 -0500

    Update stack-array-based.java

--- Data-Structures/stack-array-based.java
+++ Data-Structures/stack-array-based.java
@@ -5,16 +5,16 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-publlic class ResizeableStack<Item> implements Iterable<Item>
+class ResizeableStack<Item> implements Iterable<Item>
 {
-	Item[] arr = (Item[]) new Object[];
-	int N;
+	Item[] arr = (Item[]) new Object[1];
+	int N = 0;
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// Implement a stack using array
 	}
 	private void resize(int max) {
-		int[] temp = new int[max];
+		Item[] temp = (Item[]) new Object[max];
 		for (int i = 0; i < N; i++) 
 			temp[i] = arr[i];
 		arr = temp;
@@ -25,18 +25,18 @@ publlic class ResizeableStack<Item> implements Iterable<Item>
 	}
 	private Item pop() {
 		Item res = arr[--N];
-		arr[N] == null;
+		arr[N] = null;
 		if (N > 0 && N == arr.length / 4) resize(arr.length / 2);
 		return res;
 	}
 	public Iterator<Item> iterator() {
 		return new ResizeableStackIterator();
 	}
-	private class ResizeableStackIterator<Item> {
+	private class ResizeableStackIterator<Item> implements Iterator<Item> {
 		private int i = N;
 		public boolean hasNext() {return i < N; }
 		public Item next() {return arr[--i];}
-		public void remove() {arr[--i] = null;}
+		public void remove() {}
 	}
 	
 }
