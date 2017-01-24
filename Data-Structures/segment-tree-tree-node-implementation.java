c1eea1dc2ae90991406c2dac14ad3b83ceb6fb8a
Mai Truong <maitruong2801@gmail.com>
2017-01-24 13:40:34 -0500

    Update segment-tree-tree-node-implementation.java
    This works :)

--- Data-Structures/segment-tree-tree-node-implementation.java
+++ Data-Structures/segment-tree-tree-node-implementation.java
@@ -7,7 +7,7 @@ import java.io.*;
 /* Name of the class has to be "Main" only if the class is public. */
 class Ideone
 {
-	class SegmentTreeNode {
+	static class SegmentTreeNode {
 		int start, end; // range that the node covers
 		SegmentTreeNode left, right;
 		int sum;
@@ -21,29 +21,36 @@ class Ideone
 	}
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		SegmentTreeNode root = null;
+		int[] arr = new int[] {1, 3, 5};
+		SegmentTreeNode root = build(arr, 0, arr.length-1);
+		System.out.println(query(root, 0, 2));
+		update(root, 1, 2);
+		System.out.println(query(root, 0, 2));
 	}
 	
-	public static void build(SegmentTreeNode root, int start, int end) {
+	public static SegmentTreeNode build(int[] arr, int start, int end) {
 		SegmentTreeNode root = new SegmentTreeNode(start, end);
 		if (start == end) {
 			root.sum = arr[start];
 		} else {
 			int mid = (start + end) / 2;
-			build(root.left, start, mid);
-			build(root.right, mid+1, end);
+			root.left = build(arr, start, mid);
+			root.right = build(arr, mid+1, end);
 			root.sum = root.left.sum + root.right.sum;
-			} 
 		}
+		System.out.println("start= " + start + ", end= " + end + 
+		", sum= " + root.sum);
+		return root;
 	}
 	
-	public static void update(SegmentTreeNode root, int val, int i) {
+	public static void update(SegmentTreeNode root, int i, int val) {
+		System.out.println("start= " + root.start + ", end= " + root.end + 
+		", sum= " + root.sum);
 		if (root.start == root.end) {
 			root.sum = val;
-			arr[i] = val;
 		} else {
 			int mid = (root.start + root.end) / 2;
-			if (mid > i) {
+			if (mid >= i) {
 				update(root.left, val, i);
 			} else {
 				update(root.right, val, i);
@@ -56,7 +63,7 @@ class Ideone
 		if (root.start > right || root.end < left) {
 			return 0;
 		} 
-		if (root.end <= right && root.start >= left) {
+		if (root.end == right && root.start == left) {
 			return root.sum;
 		} 
 		int mid = (root.start + root.end) / 2;
