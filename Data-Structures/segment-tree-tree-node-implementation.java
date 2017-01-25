923a512a2fe1b94d5c02068fcb3449c7799b816f
Mai Truong <maitruong2801@gmail.com>
2017-01-25 01:49:34 -0500

    Update segment-tree-tree-node-implementation.java
    This one just got accepted in leetcode, so...

--- Data-Structures/segment-tree-tree-node-implementation.java
+++ Data-Structures/segment-tree-tree-node-implementation.java
@@ -1,13 +1,6 @@
-/* package whatever; // don't place package name! */
-
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-
-/* Name of the class has to be "Main" only if the class is public. */
-class Ideone
-{
-	static class SegmentTreeNode {
+public class NumArray {
+    
+    class SegmentTreeNode {
 		int start, end; // range that the node covers
 		SegmentTreeNode left, right;
 		int sum;
@@ -19,47 +12,58 @@ class Ideone
 			this.sum = 0;
 		}
 	}
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		int[] arr = new int[] {1, 3, 5};
-		SegmentTreeNode root = build(arr, 0, arr.length-1);
-		System.out.println(query(root, 0, 2));
-		update(root, 1, 2);
-		System.out.println(query(root, 0, 2));
-	}
-	
-	public static SegmentTreeNode build(int[] arr, int start, int end) {
-		SegmentTreeNode root = new SegmentTreeNode(start, end);
-		if (start == end) {
-			root.sum = arr[start];
+    SegmentTreeNode root;
+    public NumArray(int[] nums) {
+        root = build(nums, 0, nums.length-1);
+    }
+    
+    
+    public SegmentTreeNode build(int[] arr, int start, int end) {
+//         if (root != null) System.out.println("build1. start= " + root.start + ", end= " + root.end +	", sum= " + root.sum);
+// 		else System.out.println("build1. null, start= " + start + ", end= " + end);
+		if (start > end) { 
+			return null;
 		} else {
-			int mid = (start + end) / 2;
-			root.left = build(arr, start, mid);
-			root.right = build(arr, mid+1, end);
-			root.sum = root.left.sum + root.right.sum;
+			SegmentTreeNode root = new SegmentTreeNode(start, end);
+			if (start == end) {
+				root.sum = arr[start];
+			} else {
+				int mid = (start + end) / 2;
+				root.left = build(arr, start, mid);
+				root.right = build(arr, mid+1, end);
+				root.sum = root.left.sum + root.right.sum;
+			}
+// 			System.out.println("build2. start= " + start + ", end= " + end + ", sum= " + root.sum);
+			return root;
 		}
-		System.out.println("start= " + start + ", end= " + end + 
-		", sum= " + root.sum);
-		return root;
-	}
+	} 
 	
-	public static void update(SegmentTreeNode root, int i, int val) {
-		System.out.println("start= " + root.start + ", end= " + root.end + 
-		", sum= " + root.sum);
+	
+	public void update(SegmentTreeNode root, int i, int val) {
+// 		if (root != null) System.out.println("update1. start= " + root.start + ", end= " + root.end +	", sum= " + root.sum);
+// 		else System.out.println("update1. null");
 		if (root.start == root.end) {
 			root.sum = val;
+// 			System.out.println("update2. start= " + root.start + ", end= " + root.end + 	", sum= " + root.sum + ", val= " + val);
 		} else {
 			int mid = (root.start + root.end) / 2;
 			if (mid >= i) {
-				update(root.left, val, i);
+				update(root.left, i, val);
 			} else {
-				update(root.right, val, i);
+				update(root.right, i, val);
 			}
 			root.sum = root.left.sum + root.right.sum;
+// 			System.out.println("update3. start= " + root.start + ", end= " + root.end + 
+// 		", sum= " + root.sum);
 		}
 	}
 	
-	public static int query(SegmentTreeNode root, int left, int right) {
+    public void update(int i, int val) {
+        update(root, i, val);
+    }
+    
+    public int query(SegmentTreeNode root, int left, int right) {
+// 		System.out.println("query. start= " + root.start + ", end= " + root.end + ", sum= " + root.sum);
 		if (root.start > right || root.end < left) {
 			return 0;
 		} 
@@ -67,8 +71,25 @@ class Ideone
 			return root.sum;
 		} 
 		int mid = (root.start + root.end) / 2;
+		if (mid >= right) {
+		    return query(root.left, left, right);
+		}
+		if (mid < left) {
+		    return query(root.right, left, right);
+		}
 		int leftSum = query(root.left, left, mid);
 		int rightSum = query(root.right, mid+1, right);
 		return leftSum + rightSum;
 	}
+	
+    public int sumRange(int i, int j) {
+        return query(root, i, j);
+    }
 }
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * obj.update(i,val);
+ * int param_2 = obj.sumRange(i,j);
+ */
