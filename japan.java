713bfd80480d759bfa2708a73ae20a75256fbbc2
Mai Truong <maitruong2801@gmail.com>
2017-02-14 14:39:54 -0500

    It's working now but the ascending version.
    Needs to be converted to descending order

--- japan.java
+++ japan.java
@@ -17,35 +17,45 @@ class Ideone
 			int east = sc.nextInt(), west = sc.nextInt();
 			bridges.add(new int[] {east, west});
 		}
+		for (int i = 0; i < k; i++) 
+		// System.out.println("i= " + i + ", east= " + bridges.get(i)[0] + ", west= " + bridges.get(i)[1]);
+		// System.out.println();
 		Collections.sort(bridges, new Comparator<int[]>() {
 			public int compare(int[] bridge1, int[] bridge2) {
-				if (bridge1[0] == bridge2[0]) return bridge2[1] - bridge1[1];
-				else return bridge2[0] - bridge1[0];
+				if (bridge1[0] == bridge2[0]) return -(bridge2[1] - bridge1[1]);
+				else return -(bridge2[0] - bridge1[0]);
 			}
 		});
-		int[] BIT = new int[k];
+		for (int i = 0; i < k; i++) 
+		System.out.println("i= " + i + ", east= " + bridges.get(i)[0] + ", west= " + bridges.get(i)[1]);
+		int[] BIT = new int[20];
 		Ideone o = new Ideone();
-		System.out.println(o.solve(k, bridges, BIT));
+		System.out.println(o.solve(m, k, bridges, BIT));
 	}
-	public int solve(int k, List<int[]> bridges, int[] BIT) {
-		for (int i = 0; i < k; i++) {
-			update(k, i, 1, BIT);
-		}
+	public int solve(int m, int k, List<int[]> bridges, int[] BIT) {
 		int sum = 0;
 		for (int i = 0; i < k; i++) {
-			sum += query(k, i, BIT);
+			System.out.println("i= " + i + ", west= " + bridges.get(i)[1]);
+			System.out.println("Pre-update: " + Arrays.toString(BIT));
+			update(bridges.get(i)[1], m, BIT);
+			System.out.println("Post-update: " + Arrays.toString(BIT));
+			int sumM = query(m, BIT);
+			int sumI = query(bridges.get(i)[1], BIT);
+			sum += sumM - sumI;
+			System.out.println("sum= " + sum + ", sumM= " + sumM + ", sumI= " + sumI);
+			
 		}
 		return sum;
 	}
 	
-	public void update(int k, int index, int value, int[] BIT) {
-		while (index <= k) {
-			BIT[index] += value;
+	public void update(int index, int max, int[] BIT) {
+		while (index <= max) {
+			BIT[index] += 1;
 			index += index & (-index);
 		}
 	}
 	
-	public int query(int k, int index, int[] BIT) {
+	public int query(int index, int[] BIT) {
 		int ans = 0;
 		while (index > 0) {
 			ans += BIT[index];
