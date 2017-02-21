85714eba59708fab70d3737e16a0e4afdaceb0f3
Mai Truong <maitruong2801@gmail.com>
2017-02-21 12:01:10 -0500

    Update components-in-a-graph.java

--- components-in-a-graph.java
+++ components-in-a-graph.java
@@ -10,22 +10,23 @@ public class Solution {
         int n = sc.nextInt();
         int g[] = new int[n+1];
         int b[] = new int[n+1];
+        UF uf = new UF();
+        uf.init(g, b, n);
         for (int i = 1; i <=n; i++) {
             g[i] = sc.nextInt();
             b[i] = sc.nextInt();
-            union(g[i], g[i-1]);
-            union(b[i], b[i-1]);
-            union(g[i], b[i]);
+            uf.union(g, b, g[i], g[i-1]);
+            uf.union(g, b, b[i], b[i-1]);
+            uf.union(g, b, g[i], b[i]);
         }
+        System.out.println(UF.maxSize + " " + UF.minSize);
     }
     
     static class UF {
         int parent[] = new int[30001];
         int size[] = new int[30001];
-        public UF(int n) {
-            init(n);
-        }
-        public void init(int n) {
+        static int maxSize = 0, minSize = Integer.MAX_VALUE;
+        public void init(int g[], int b[], int n) {
             for (int i = 0; i < n; i++) {
                 parent[g[i]] = g[i];
                 size[g[i]] = 1;
@@ -33,16 +34,20 @@ public class Solution {
                 size[b[i]] = 1;
             }
         }
-        public void union(int u, int v) {
+        public void union(int g[], int b[], int u, int v) {
             int i = find(u);
             int j = find(v);
             if (i == j) return;
             if (size[u] > size[v]) {
                 parent[v] = parent[u];
                 size[u] += size[v];
+                if (size[u] > maxSize) maxSize = size[u];
+                if (size[u] < minSize) minSize = size[u];
             } else {
                 parent[u] = parent[v];
                 size[v] += size[u];
+                if (size[v] > maxSize) maxSize = size[v];
+                if (size[v] < minSize) minSize = size[v];
             }
         }
         public int find(int u) {
