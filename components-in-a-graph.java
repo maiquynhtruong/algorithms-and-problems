bba797826e60d77fa88392b1df500a77b3746fcc
Mai Truong <maitruong2801@gmail.com>
2017-02-25 18:24:47 -0800

    Update components-in-a-graph.java

--- components-in-a-graph.java
+++ components-in-a-graph.java
@@ -8,47 +8,46 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int g[] = new int[n+1];
-        int b[] = new int[n+1];
         UF uf = new UF();
-        uf.init(g, b, n);
+        uf.init(2*n);
         for (int i = 1; i <=n; i++) {
-            g[i] = sc.nextInt();
-            b[i] = sc.nextInt();
-            uf.union(g, b, g[i], g[i-1]);
-            uf.union(g, b, b[i], b[i-1]);
-            uf.union(g, b, g[i], b[i]);
+            int g = sc.nextInt();
+            int b = sc.nextInt();
+            uf.union(g, b);
         }
-        System.out.println(UF.maxSize + " " + UF.minSize);
+        int maxSize = Integer.MIN_VALUE, minSize = Integer.MAX_VALUE;
+        for (int i = 1; i <= 2*n; i++) {
+            int p = uf.find(i);
+            int s = uf.size[p];
+            if (s > 1) {
+                if (minSize > s) minSize = s;
+                if (maxSize < s) maxSize = s;
+            }
+        }
+        System.out.println(minSize + " " + maxSize);
     }
     
     static class UF {
-        int parent[] = new int[30001];
-        int size[] = new int[30001];
-        static int maxSize = 0, minSize = Integer.MAX_VALUE;
-        public void init(int g[], int b[], int n) {
-            for (int i = 0; i < n; i++) {
-                parent[g[i]] = g[i];
-                size[g[i]] = 1;
-                parent[b[i]] = b[i];
-                size[b[i]] = 1;
+    	static final int MAXN = 30001;
+        int parent[] = new int[MAXN];
+        int size[] = new int[MAXN];
+        
+        public void init(int n) {
+            for (int i = 1; i <= n; i++) {
+                parent[i] = i;
+                size[i] = 1;
             }
         }
-        public void union(int g[], int b[], int u, int v) {
+        public void union(int u, int v) {
             int i = find(u);
             int j = find(v);
+            // System.out.println("u= " + u + ", v= " + v + ", i= " + i + ", j= " + j);
             if (i == j) return;
-            if (size[u] > size[v]) {
-                parent[v] = parent[u];
-                size[u] += size[v];
-                if (size[u] > maxSize) maxSize = size[u];
-                if (size[u] < minSize) minSize = size[u];
-            } else {
-                parent[u] = parent[v];
-                size[v] += size[u];
-                if (size[v] > maxSize) maxSize = size[v];
-                if (size[v] < minSize) minSize = size[v];
-            }
+            parent[i] = j;
+            size[j] += size[i];
+            // System.out.println("size[" + j + "]= " + size[j]);
+            // if (size[j] > maxSize) maxSize = size[j];
+            // if (size[j] < minSize) minSize = size[j];
         }
         public int find(int u) {
             if (parent[u] == u) return u;
