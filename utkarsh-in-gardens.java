dbed376824a35d06f555db2c9cc49f3121cc82a7
Mai Truong <maitruong2801@gmail.com>
2017-03-03 20:36:10 -0800

    Update and rename Utkarsh-in-gardens.java to utkarsh-in-gardens.java

--- utkarsh-in-gardens.java
+++ utkarsh-in-gardens.java
@@ -16,19 +16,15 @@ class Ideone
 			for (int j = 0; j < n; j++) {
 				a[i][j] = sc.nextInt();
 			}
-		int d[][] = new int[n+1][n+1];
-		for (int i = 1; i <= n; i++)
+		int ans = 0;
+		for (int i = 0; i < n; i++)
 			for (int j = i+1; j <= n; j++) {
 				int cnt = 0;
-				for (int k = 1; k <= n; k++) 
+				for (int k = 0; k < n; k++) 
 					if (a[i][k]==1 && a[k][j]==1) cnt++;
-				d[i][j] += cnt;
+				ans += cnt*(cnt-1)/2;
 			}
-		int ans = 0;
-		for (int i = 1; i <= n; i++)
-			for (int j = 1; j <= n; j++) {
-				ans += d[i][j]*(d[i][j] - 1)/2;
-			}
+		
 		System.out.println(ans/2);
 	}
 }
