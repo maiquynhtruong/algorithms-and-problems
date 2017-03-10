7b505f0345c986d4ddc4a5b35a5ecd8228a8cc1f
Mai Truong <maitruong2801@gmail.com>
2017-03-09 20:49:50 -0800

    Update friends-and-foes.java

--- friends-and-foes.java
+++ friends-and-foes.java
@@ -7,10 +7,25 @@ import java.io.*;
 /* Name of the class has to be "Main" only if the class is public. */
 class Ideone
 {
+	int size[], parent[];
+	public static void union(int a, int b) {
+		int u = find(a);
+		int v = find(b);
+		if (u == v) return;
+		if (size[u] > size[v]) {
+			parent[v] = u;
+			size[u] += size[v];
+		} else {
+			parent[u] = v;
+			size[v] += size[u];
+		}
+	}
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
+		size = new int[n];
+		parent = new int[n];
 		int m1 = sc.nextInt();
 		while (m1--) {
 			int a = sc.nextInt(), b = sc.nextInt();
@@ -20,7 +35,7 @@ class Ideone
 		while (m2--) {
 			int a = sc.nextInt(), b = sc.nextInt();
 			if (find(a) == find(b)) {
-				
+				size[find(a)] = size[find(b)] = 0;
 			}
 		}
 	}
