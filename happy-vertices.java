7b07dbf5f4c0d8d1582e00cfb0718a23b0394388
Mai Truong <maitruong2801@gmail.com>
2017-03-11 11:54:20 -0800

    Update happy-vertices.java

--- happy-vertices.java
+++ happy-vertices.java
@@ -5,31 +5,52 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
+/*
+The problem is simple implementation of DFS while maintaining a count of children 
+of the vertices visited. Let u be a vertex and v be its child. Then, check if 
+children[v]>children[u].This way, check for each vertex while traversing the graph in DFS.
+One important thing to note that actually there are many trees in the input(a graph with 
+no cycles and self loops is a tree). That means , you have to apply DFS for each of these trees.
+*/
 class Ideone
 {
+	static int[] parent;
+	static int[] children;
+	static boolean[] visited;
+	static List<List<Integer>> nodes;
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt(), m = sc.nextInt();
+		nodes = new ArrayList<List<Integer>>();
+		parent = new int[n+1];
+		children = new int[n+1];
+		visited = new boolean[n+1];
+		for (int i = 0; i <= n; i++) nodes.add(new ArrayList<Integer>());
 		for (int i = 0; i < m; i++) {
 			int x = sc.nextInt(), y = sc.nextInt();
+			nodes.get(x-1).add(y-1);
 		}
-		for (int i = 0; i < m; i++) 
-			if (!visited[i]) dfs(i);
-	}
-	int static cnt = 0;
-	public static void dfs(int u) {
-		int edges = 0, children = 0;
 		for (int i = 0; i < n; i++) {
-			edges++;
-			if (!visited[i] && a[u][i]) {
-				children++;
+			if (!visited[i]) {
+				parent[i] = -1;
 				dfs(i);
 			}
 		}
-		if (children > edges - children) {
-			cnt++;
+		int cnt = 0;
+		for (int i = 0; i <= n; i++)
+			if (parent[i] != -1)
+				if (children[i] > children[parent[i]]) cnt++;
+		System.out.println(cnt);
+	}
+	public static void dfs(int u) {
+		visited[u] = true;
+		for (int v : nodes.get(u)) {
+			if (!visited[v]) {
+				children[u]++;
+				parent[v] = u;
+				dfs(v);
+			}
 		}
 	}
 }
