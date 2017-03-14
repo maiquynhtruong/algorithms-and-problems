79a11f6ab85ff35f7f56ccd0584c5a26d0b48da7
Mai Truong <maitruong2801@gmail.com>
2017-03-13 21:12:07 -0700

    Update prim.java

--- classical-problems/prim.java
+++ classical-problems/prim.java
@@ -1,1 +1,24 @@
 
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Edge {
+	int u; int v; int w;
+	public Edge(int u, int v, int w) {
+		this.u = u;
+		this.v = v;
+		this.w = w;
+	}
+	
+}
+class Ideone
+{
+	
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+	}
+}
