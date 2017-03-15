bbd05ae408b86bef53ac87136b8c8fdcf0e6e4d3
Mai Truong <maitruong2801@gmail.com>
2017-03-14 20:57:43 -0700

    Update prim.java

--- classical-problems/prim.java
+++ classical-problems/prim.java
@@ -1,24 +1,32 @@
-
 /* package whatever; // don't place package name! */
 
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class Edge {
-	int u; int v; int w;
-	public Edge(int u, int v, int w) {
-		this.u = u;
-		this.v = v;
-		this.w = w;
-	}
-	
-}
 class Ideone
 {
 	
+	int[][] neighbors; // neighbors vertices with weights
+	int[] parent;
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		
+		Scanner sc = new Scanner(System.in);
+		int m = sc.nextInt(); // edges
+		int n = sc.nextInt(); // vertices
+		parent = new int[n];
+		neighbors = new int[n][n];
+		PriorityQueue<Edge> queue = new PriorityQueue<Edge>();
+		for (int i = 0; i < m; i++) {
+			int u = sc.nextInt()-1;
+			int v = sc.nextInt()-1;
+			int w = sc.nextInt();
+			neighbors[u][v] = w;
+			neighbors[v][u] = w;
+		}
+		while (!queue.empty()) {
+			int v = queue.pop();
+			
+		}
 	}
 }
