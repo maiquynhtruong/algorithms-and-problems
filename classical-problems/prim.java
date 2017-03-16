35fbaaa2a5dfbbecb0976c00abea78f024aad7d7
Mai Truong <maitruong2801@gmail.com>
2017-03-15 20:59:37 -0700

    Update prim.java

--- classical-problems/prim.java
+++ classical-problems/prim.java
@@ -8,7 +8,9 @@ class Ideone
 {
 	
 	int[][] neighbors; // neighbors vertices with weights
-	int[] parent;
+	int[] parent; // preceding node in the tree
+	int[] dist; // shortest distance to any vertex in MST
+	boolean[] visited; // true if the vertex has been considered
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc = new Scanner(System.in);
@@ -16,7 +18,7 @@ class Ideone
 		int n = sc.nextInt(); // vertices
 		parent = new int[n];
 		neighbors = new int[n][n];
-		PriorityQueue<Edge> queue = new PriorityQueue<Edge>();
+		// PriorityQueue<Edge> queue = new PriorityQueue<Edge>();
 		for (int i = 0; i < m; i++) {
 			int u = sc.nextInt()-1;
 			int v = sc.nextInt()-1;
@@ -24,9 +26,28 @@ class Ideone
 			neighbors[u][v] = w;
 			neighbors[v][u] = w;
 		}
-		while (!queue.empty()) {
-			int v = queue.pop();
+		for (int i = 0; i < n; i++) dist[i] = Integer.MAX_VALUE;
+		dist[s] = 0;
+		for (int i = 0; i < dist.length; i++) {
+			int next = minVertex(); // the smallest vertex in non-MST subset
+			visited[next] = true; // next has been included in MST
+			for (int v = 0; v < dist.length; v++) {
+				
+			}
 			
 		}
+		
+	}
+	
+	public static int minVertex() {//int[] dist, boolean[] visited) {
+		int w = Integer.MAX_VALUE;
+		int i = -1;
+		for (int v = 0; v < dist.length; v++) {
+			if (!visited[v] && dist[v] < w) {
+				w = dist[v];
+				i = v;
+			}
+		}
+		return i;
 	}
 }
