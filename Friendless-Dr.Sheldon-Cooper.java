67aeb21f89763bf0c79c4d6c86115621c4cb0e77
Mai Truong <maitruong2801@gmail.com>
2017-03-16 20:40:54 -0700

    Create Friendless-Dr.Sheldon-Cooper.java

---
+++ Friendless-Dr.Sheldon-Cooper.java
@@ -0,0 +1,10 @@
+// https://www.hackerearth.com/practice/algorithms/graphs/minimum-spanning-tree/practice-problems/algorithm/friendless-dr-sheldon-cooper-14/
+
+class Solution {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+    }
+}
