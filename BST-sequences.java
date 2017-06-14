8af31570e89538782f62dadfab6bf62dd0da9c1e
Mai Truong <maitruong2801@gmail.com>
2017-06-14 09:49:09 -0400

    Create BST-sequences.java

--- BST-sequences.java
+++ BST-sequences.java
@@ -7,3 +7,21 @@ Example:
  1   3
  Output: {2, 1, 3}, {2, 3, 1}
  **/
+public class Solution {
+     public void BSTSequences (TreeNode root, List<Integer> curList) {
+//         List<Integer> curList = new ArrayList<Integer>();
+        if (root == null) return;
+        curList.add(root);
+        if (root.left != null) curList.add(root.left);
+        if (root.right != null) curList.add(root.right);
+        BSTSequences(root.left, curList);
+        BSTSequences(root.right, curList);
+        if (root.right != null) curList.add(root.right);
+        if (root.left != null) curList.add(root.left);
+        BSTSequences(root.right, curList);
+        BSTSequences(root.left, curList);
+     }
+    public static void main(String args[]) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();   
+    }
+}
