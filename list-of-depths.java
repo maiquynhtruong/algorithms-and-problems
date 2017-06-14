98e227585e6c33077a96835a59557140c472376b
Mai Truong <maitruong2801@gmail.com>
2017-06-14 13:57:01 -0400

    Create list-of-depths.java

--- list-of-depths.java
+++ list-of-depths.java
@@ -8,6 +8,23 @@ public class Solution {
         listOfDepths(root.left, level+1);
         listOfDepths(root.right, level+1);
     }
+    
+    List<List<TreeNode>> lists2 = new ArrayList<List<TreeNode>>();
+    public static void listOfDepths(TreeNode root, int level) {
+        if (root == null) return;
+        List<TreeNode> current = new ArrayList<TreeNode>();
+        current.add(root);
+        while (current.size() > 0) {
+            lists2.add(current);
+            ArrayList<TreeNode> parents = current;
+            current = new ArrayList<TreeNode>();
+            for (TreeNode parent : parents) {
+                if (parent.left != null) current.add(parent.left);
+                if (parent.right != null) current.add(parent.right);
+            }
+        }
+    }
+    
     public static void main(String args[]) {
         lists = new ArrayList<List<Integer>>();
         listsOfDepth(root, 0);
