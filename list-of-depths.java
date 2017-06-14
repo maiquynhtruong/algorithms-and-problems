d50301098d1d957d4574d88e608d47a0df8be774
Mai Truong <maitruong2801@gmail.com>
2017-06-14 13:38:49 -0400

    Create list-of-depths.java

--- list-of-depths.java
+++ list-of-depths.java
@@ -1,1 +1,15 @@
 /** Given a binary tree, create a linked list of all nodes at each depth **/
+public class Solution {
+    List<List<Integer>> lists;
+    public static void listOfDepths(TreeNode root, int level) {
+        lists.get(level).add(root.left);
+        lists.get(level).add(root.right);
+        listOfDepths(root.left, level+1);
+        listOfDepths(root.right, level+1);
+    }
+    public static void main(String args[]) {
+        lists = new ArrayList<List<Integer>>();
+        listsOfDepth(root, 0);
+    }
+  
+}
