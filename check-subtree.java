c3f653140b8a5806c693ccdb602130a5ff676931
Mai Truong <maitruong2801@gmail.com>
2017-06-14 09:07:36 -0400

    Create check-subtree.java

---
+++ check-subtree.java
@@ -0,0 +1,3 @@
+/** T1 and T2 are two very large binary tree, with T1 much bigger than T2. Determin if T2 is a subtree of T1.
+A tree T2 is a subtree of T1 if there exits a node n in T1 such that the subtree of n is identical to T2. 
+If you cut off the tree at node n, the two trees would be identical. **/
