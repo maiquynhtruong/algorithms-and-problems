42abd695e9ffb7b3157c6984072388455d1d193a
Mai Truong <maitruong2801@gmail.com>
2017-06-14 15:38:33 -0400

    Create validate-BST.java

--- validate-BST.java
+++ validate-BST.java
@@ -1,1 +1,10 @@
 /**implement a function to check if a binary tree is a BST **/
+
+public class Solution {
+    public static boolean checkBST(TreeNode root) {
+      
+    }
+    public static void main(String args[]) {
+
+    }
+}
