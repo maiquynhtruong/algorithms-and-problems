fa64619c1231eefe0a14126fa7e507681a4b6c5f
Mai Truong <maitruong2801@gmail.com>
2017-06-14 09:05:33 -0400

    Create BST-sequences.java

---
+++ BST-sequences.java
@@ -0,0 +1,9 @@
+/** A binary search tree was cretated by traversing thorugh an array from left to right and inserting each element. 
+Given a BST with distinct elements, print all possible arrys that oculd have led to this tree.
+Example:
+
+   2
+  / \
+ 1   3
+ Output: {2, 1, 3}, {2, 3, 1}
+ **/
