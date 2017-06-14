b4d6fae9f0bffbfd8416b48955de84707a602d55
Mai Truong <maitruong2801@gmail.com>
2017-06-14 15:37:18 -0400

    Create successor.java

--- successor.java
+++ successor.java
@@ -1,1 +1,27 @@
 /** Find the "next" node (in-order successor) of a given node in a binary search tree. Each node has a link to its parent**/
+public class TreeNode {
+    int val;
+    TreeNode left, right, parent;
+    public TreeNode(int val) {this.val = val);
+}
+public class Solution {
+    public static TreeNode findSuccessor(TreeNode root) {
+        if (root.right != null) return findLeftmostNode(root.right);
+        else {
+            TreeNode parent = root.parent;
+            while (parent.val < root.val) {
+                parent = parent.parent;   
+            }
+            return parent;
+        }
+    }
+    
+    public static TreeNode findLeftmostNode(TreeNode root) {
+        if (root.left == null) return root;   
+        while (root.left != null) root = root.left;
+        return root;
+    }
+    public static void main(String args[]) {
+           
+    }
+}
