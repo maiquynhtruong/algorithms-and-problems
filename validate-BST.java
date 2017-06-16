1e36fbbd28410c0e0eb4de495e637b44f287f50c
Mai Truong <maitruong2801@gmail.com>
2017-06-16 14:54:18 -0400

    Update validate-BST.java

--- validate-BST.java
+++ validate-BST.java
@@ -7,8 +7,17 @@ public class Solution {
     public static boolean checkBSTWithRange(TreeNode root, int min, int max) {
         if (root.left.val > root.val) return false;
         if (root.right.val < root.val) return false;
-        if (!checkBSTWithRange(root.left, max, root.val)) return false;
+        if (!checkBSTWithRange(root.left, min, root.val)) return false;
         if (!checkBSTWithRange(root.right, root.val, max)) return false;
+        return true;
+    }
+    Integer last_data = null;
+    public static boolean checkBST(TreeNode root) {
+        if (root == null) return true;
+        if (!checkBST(root.left)) return false;
+        if (last_data != null && root.val >= last_data) return false;
+        last_data = root.val;
+        if (!checkBST(root.right)) return false;
         return true;
     }
     public static void main(String args[]) {
