db85bba993c2654a92b0d80daa9491b6d0f9f9bb
Mai Truong <maitruong2801@gmail.com>
2017-06-24 12:50:36 -0700

    Update peaks-and-valleys.java

--- peaks-and-valleys.java
+++ peaks-and-valleys.java
@@ -4,10 +4,23 @@ public class Solution {
     5
     public static int[] peaksAndValleys(int[] a) {
         for (int i = 1; i < a.length; i += 2) {
-            if (a[i-1] > a[i]) {
-                swap(arr, i-1, i);   
-            } 
+            int maxIndex = findMaxIndex(a, i-1, i, i+1);
+            if (i != maxIndex) {
+                swap(a, i, maxIndex);
+            }
         }
+    }
+    
+    public static int findMaxIndex(int[] a, int i, int j, int k) {
+        int len = a.length;
+        int iVal, jVal, kVal;
+        if (0 <= i && i < len) iVal = a[i] else iVal = Integer.MIN_VALUE;
+        if (0 <= j && j < len) jVal = a[j] else jVal = Integer.MIN_VALUE;
+        if (0 <= k && k < len) kVal = a[k] else kVal = Integer.MIN_VALUE;
+        int max = Math.max(iVal, Math.max(jVal, kVal));
+        if (max == iVal) return i;
+        else if (max == jVal) return j;
+        else return k;
     }
     public static void main(String args[]) {
         
