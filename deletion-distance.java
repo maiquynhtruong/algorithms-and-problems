1ce5a5722a1b7e9c67e4df6bcce4fab4d2b1644e
Mai Truong <maitruong2801@gmail.com>
2017-06-24 06:26:45 -0700

    Update deletion-distance.java

--- deletion-distance.java
+++ deletion-distance.java
@@ -16,8 +16,27 @@ class Solution {
         return dp[str1.length()][str2.length()];
     }
 }
-  
-
+    int len = Math.max(str1.length(), str2.length()];
+    int cur[] = new int[len+1];
+    int prev[] = new int[len+1];
+    static int deletionDistance(String str1, String str2) {
+        for (int i = 0; i < str1.length(); i++) {
+            for (int j = 0; j < str2.length(); j++) {
+                if (i == 0) {
+                    cur[j] = j;
+                } else if (j == 0) {
+                    cur[j] = i;
+                } else if (str1.charAt(i) == str2.charAt(j)) {
+                    cur[j] = prev[j-1];
+                } else {
+                    cur[j] = 1 + Math.min(cur[j-1], prev[j]);
+                }
+                prev = cur;
+                cur = new int[len+1];
+            }
+        }
+        return prev[len];        
+    }
   public static void main(String[] args) {
       String str1 = "some", str2 = "thing";
       System.out.println(deletionDistance(str1, str2));
