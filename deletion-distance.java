94f4b7f82c6d5ad5069a3266594971151d597278
Mai Truong <maitruong2801@gmail.com>
2017-06-24 06:11:14 -0700

    Update deletion-distance.java

--- deletion-distance.java
+++ deletion-distance.java
@@ -2,41 +2,19 @@ import java.io.*;
 import java.util.*;
 
 class Solution {
-
-  static int deletionDistance(String str1, String str2) {
-    int[] charSet1 = new int[27]; // for 26 charcters
-      for (int i = 0; i < str1.length(); i++) {
-          charSet1[str1.charAt(i) - 'a']++;
-      }
-    System.out.println(Arrays.toString(charSet1));
-    int uniqueTo2 = 0;
-    StringBuilder s2 = new StringBuilder(str2);
-      for (int i = 0; i < str2.length(); i++) {
-          if (charSet1[str2.charAt(i) - 'a'] > 0) {
-            charSet1[str2.charAt(i) - 'a']--;
-            if (charSet1[str2.charAt(i) - 'a'] == 0){
-                uniqueTo2++;
-                System.out.print(str2.charAt(i) + " ");
-            } 
-          }
-      }
-    System.out.println();
-    int[] charSet2 = new int[27];
-    for (int i = 0; i < s2.length(); i++) {
-          charSet2[str2.charAt(i) - 'a']++;
-      }
-    int uniqueTo1 = 0;
-    StringBuilder s1 = new StringBuilder(str1);
-    for (int i = 0; i < str1.length(); i++) {
-          if (charSet2[str1.charAt(i) - 'a'] > 0) {
-            charSet2[str1.charAt(i) - 'a']--;
-            if (charSet2[str1.charAt(i) - 'a'] == 0) {
-              uniqueTo1++;
-              System.out.print(str1.charAt(i) + " ");
-            }
-          }
-      }
-    return str1.length() + str2.length() - uniqueTo1 - uniqueTo2;
+    
+    static int deletionDistance(String str1, String str2) {
+        for (int i = 0; i < str1.length(); i++) dp[i][0] = i;
+        for (int j = 0; j < str2.length(); j++) dp[0][j] = j;
+        for (int i = 0; i < str1.length(); i++)
+            for (int j = 0; j < str2.length(); j++)
+                if (str1.charAt(i) == str2.charAt(j)) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i][j-1], dp[i-1][j]);
+                }
+        return dp[str1.length()][str2.length()];
+    }
 }
   
 
