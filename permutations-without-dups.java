f6770d85605d4e1d6d5e416781f8f7b89d8afb2a
Mai Truong <maitruong2801@gmail.com>
2017-06-27 14:58:46 -0400

    Update permutations-without-dups.java

--- permutations-without-dups.java
+++ permutations-without-dups.java
@@ -18,9 +18,25 @@ public class Solution {
     
     public static String createPermutation(String word, int i, char c) {
             String pre = word.substring(0, i);
-            String suf = word.substring(i+1);
+            String suf = word.substring(i);
             return pre + c + suf;
     }
+    
+    public ArrayList<String> perm(String s) {
+        if (s == null) return null;
+        ArrayList<String> res = new ArrayList<String>();
+        if (s.length() == 0) {
+            res.add("");
+            return res;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            ArrayList<String> permOfRest = perm(s.substring(0,i) + s.substring(i+1));
+            for (String p : permOfRest) res.add(c + p);
+        }
+        return res;
+    }
+    
     public static void main (String args[]) {
         
     }
