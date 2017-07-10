92577b049394c346346639e70c74f4a86934625f
Mai Truong <maitruong2801@gmail.com>
2017-07-10 16:12:31 -0400

    Update chat-server.java

--- chat-server.java
+++ chat-server.java
@@ -15,20 +15,23 @@ public class Solution {
             receiver.receivedAddRequest(sender);
         } 
         
-        public void acceptRequest(AddRequest request) {}
+        public void acceptRequest(AddRequest request) { 
+            request.receiver.contacts.add(sender);
+            request.receiver.deleteRAddRequest(this);
+        }
         public void rejectRequest(AddRequest request) {}
-        public void sendRequest(String message, User receiver) {}
     }
     
     public class AddRequest {
+        public Date = 
         User sender, receiver;
         boolean accepted;
     }
-    public enum UserStatus {
+    public enum UserStatusType {
         ONLINE, OFFLINE, IDLE, AWAY, BUSY
     }
     public class User {
-        String name;
+        
         // maps user id with add requests received
         HashMap<Integer, AddRequest> receivedAddRequest = new HashMap<Integer, AddRequest>(); 
         // maps user id with add requests sent
@@ -36,17 +39,59 @@ public class Solution {
         UserStatus status = null;
         private HashMap<Integer, PrivateChat> privateChats; // one to one chat
         private ArrayList<GroupChat> groupChats;
-        public requestAddUser(User receiver) {
-            UserManager.getInstance().addUser(receiver, this);
-        }
-        public void receivedAddRequest(User sender) {
-            addRequests.add(sender);
-            // make decision
-        }
-        
+        HashMap<Integer, User> contacts;
+        public int id;
+        public username;
+        public String fullName;
+        public void requestAddUser(User receiver) { UserManager.getInstance().addUser(receiver, this); }
+        public void receivedAddRequest(User sender) { addRequests.add(sender); }
+        public void sendAddRequest(AddRequest request){}
+        public void deleteAddRequest(AddRequest request) {}
+        public void sendMessageToUser(User receiver) {}
+        public void sendMessageToGroupChat(GroupChat group) {}
+        public void setStatus(UserStatus status) { this.status = status; }
+        public UserStatus getStatus() { return status; }
+        public void addContact(User user) {}
+        public void setFullName(String newName) {}
+        public void setProfilePicture() {}
+        public GroupChat createGroupChat(List<User> users) {}
+        public int getId() { return id; }
+        public String getUsername() { return username; }
+        public String getFullName() { return fullName; }        
     }
     
-    public class Server {
+    public abstract class Conversation {
+        List<User> participants;
+        HashMap<Integer, List<Message>> messages; // maps list of message a user sent
+        public int id;
+        public List<String> getMessageFromUser(int userId) { return messages.get(userId); }
+        public List<String> getMessagesFromAll() { return; ...}
+        public boolean addMessage(int userId, Message mes) {}
+        public int getId() { return id; }
+    }
     
+    public PrivateChat extends Conersation {
+        User user1, user2;
+        public PrivateChat(User user1, User user2) {}
+        public boolean addMessage(int userId, Message mes ) { messages.get(userId).add(mes) }; 
+    }
+    public GroupChat extends Conversation {
+        public void addMoreUsers() {}
+        public void removeUser (User user); 
+    }
+    
+    public class Message () {
+        public String message;
+        User sender, receiver;
+        private Date date;
+        public String getContent();
+        public Date getDate();
+    }
+    public claass addRequest {
+        User fromUser, toUser;
+        public RequestStatus(get s);
+    }
+    public enum requests {
+        Unread, Read, In.ccepted
     }
 }
