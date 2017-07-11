5a5652916ee06420b4d91e9ff16d5e6bf96ea8ee
Mai Truong <maitruong2801@gmail.com>
2017-07-10 20:17:33 -0400

    Create file-system.java

---
+++ file-system.java
@@ -0,0 +1,16 @@
+// Design a file system
+public class Solution {
+    public class TreeNode {
+        List<TreeNode> children;
+        int inode;
+        public TreeNode(int inodeVal) {
+            this.inode = inodeVal;
+            children = new ArrayList<TreeNode>();
+        }
+    }
+    
+    public static void main(String args[]) {
+    
+    }
+
+}
