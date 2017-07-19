2f3729961f71c15ad391f51b5dd993789cecdf05
Mai Truong <maitruong2801@gmail.com>
2017-07-19 14:57:50 -0400

    Update finite-state-machine.java
    add stack-based FSM

--- finite-state-machine.java
+++ finite-state-machine.java
@@ -53,3 +53,80 @@ public class Ant {
         brain.update();   
     }
 }
+
+/**** Implemenation using stack **/
+public class FSM {
+    /* The top of the stack contains the active state and transitions are handled by pushing or
+    popping states from the stack */
+    public Ant ant = new Ant();
+    public Stack<State> stack = new Stack<State>();
+    public void update() {
+        State activeState = stack.peek();
+        if (activeState == State.FIND_LEAF) ant.findLeaf();
+        else if (activeState == State.GO_HOME) ant.goHome();
+        else if (activeState == State.RUN_AWAY) ant.runAway();
+    }
+    /* A state can pop itself, which means the current state is complete and the next state in teh stack
+    should become active */
+    public State popState() {
+        return stack.pop();   
+    }
+    /* Currently active state will change for a while, but then it pops itself from the stack,
+    the previously active state will take over again */
+    public void pushState(State state) {
+        if (stack.peek() != state) stack.push(state);
+    }
+    
+    /* Also a state can pop itself and push another state, which means a full transition */
+}
+
+public class Ant {
+    FSM brain;
+    Vector3D velocity;
+    Vector3D position;
+    /* The "find leaf" state where the ant moves toward the leaf */
+    public void findLeaf() {
+        velocity = new Vector3D(leaf.x - position.x, leaf.y - position.y);
+        if (distance(leaf, this) <= 5) {
+            // Near the leaf so eat it and go home
+            eatLeaf();
+            brain.popState();
+            brain.pushState(State.GO_HOME);
+        }
+        if (distance(cursor, this) <= MOUSE_THREAD_RADIUS) {
+            // The "runAway" state is pushed on top of "findLeaf", which means
+            // the "findLeaf" state will be active again when "runAway" ends.
+            brain.pushState(State.RUN_AWAY);
+        }
+    }
+    
+    /* The "go home" state where the ant goes home */
+    public void goHome() {
+        velocity = new Vector3D(home.x - position.x, home.y - position.y);
+        if (distance(home, this) <= 5) {
+            // At home and needs to go find leaf
+            brain.popState();
+            brain.pushState(State.FIND_LEAF);
+        }
+        if (distance(cursor, this) <= MOUSE_THREAD_RADIUS) {
+            // The "runAway" state is pushed on top of "goHome", which means
+            // the "goHome" state will be active again when "runAway" ends.
+            brain.pushState(State.RUN_AWAY);
+        }
+    }
+    
+    /* The "run away" state where the ant runs away from the cursor */
+    public void runAway() {
+        velocity = new Vector3D(cursor.x - position.x, cursor.y - position.y);
+        moveOneStep();
+        if (distance(cursor, this) > MOUSE_THREAT_RADIUS) {
+            brain.popState();  
+        }
+    }
+    
+    /* call every game frame to determine what the ant should be doing next */
+    public void update() {
+        brain.update();   
+    }
+}
+
