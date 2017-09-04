ad39144ea23812d3f393a62f721b3b10547f4bfb
Mai Truong <maitruong2801@gmail.com>
2017-09-03 23:16:55 -0400

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,16 @@
+/***** template to use for all the code *****/
+#include <bits/stdc++.h> // include all the libraries
+using namespace std;
+// global variables
+int n;
+int main() {
+    ifstream cin("input.inp"); // take input from a file in the same directory
+    ios_base::sync_with_stdio(false); // these two lines will make cin, cout run as fast as scanf and printf
+    cin.tie(NULL);
+    cin >> n;
+    // the main code
+
+    int ans;
+    cout << ans << "\n";
+    return 0;
+}
