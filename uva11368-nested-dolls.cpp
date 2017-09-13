9eb6bab2335380fa2687cb4aa435e378e127ba10
Mai Truong <maitruong2801@gmail.com>
2017-09-13 16:20:43 -0400

    Update uva11368-nested-dolls.cpp

--- uva11368-nested-dolls.cpp
+++ uva11368-nested-dolls.cpp
@@ -1,34 +1,56 @@
 #include <bits/stdc++.h> 
 using namespace std;
-bool debug = true;
+bool debug = false;
 int m, testcases, car;
-int LIS[20005], dolls[20005][3];
+int LIS[20005], cnt[20005];
 int main() {
-    ifstream cin("nested_dolls.inp");
+    //ifstream cin("nested_dolls.inp");
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cin >> testcases;
     while (testcases--) {
     	cin >> m;
+	vector< pair<int, int> > dolls;
+    	dolls.push_back(make_pair(0, 0));
     	for (int i = 1; i <= m; i++) {
     		int w, h;
     		cin >> w >> h;
-    		dolls[i][1] = w; dolls[i][2] = h;
+    		dolls.push_back(make_pair(w, h));
+    		LIS[i] = 1; cnt[i] = 0;
     	}
-    	sort(dolls, dolls+m);
+    	
+    	if (debug) {
+    		for (int i = 1; i <= m; i++) 
+    		cout << "(" << dolls[i].first << ", " << dolls[i].second << ") ";
+    		cout << "\n";
+    	}
+    	sort(dolls.begin(), dolls.end());
     	LIS[1] = 1;
     	for (int i = 1; i <= m; i++) {
     		for (int j = 1; j < i; j++) {
-    			if (dolls[j][2] < dolls[i][2] && LIS[j]+1 > LIS[i]) {
+    			if (dolls[j].first < dolls[i].first && 
+    			dolls[j].second < dolls[i].second && 
+    			LIS[j]+1 > LIS[i]) {
     				LIS[i] = LIS[j]+1;
     			}
     		}
     	}
+    	if (debug) {
+    		for (int i = 1; i <= m; i++) cout << LIS[i] << " ";
+    		cout << "\n";
+    	}
     	sort(LIS, LIS+m);
-    	int ans = 0, i = 1;
-    	while (m > 0) {
-    		ans++;
-    		m -= LIS[i++];
+    	for (int i = 1; i <= m; i++) cnt[LIS[i]]++;
+    	if (debug) {
+    		for (int i = 1; i <= m; i++) cout << cnt[i] << " ";
+    		cout << "\n";
+    	}
+    	int ans = 0, num = m;
+    	while (num > 0) {
+	    	ans++;
+	    	for (int i = 1; i <= m; i++) {
+	    		if (cnt[i] > 0) {num--; cnt[i]--;}
+	    	}
     	}
     	cout << ans << "\n";
     }
