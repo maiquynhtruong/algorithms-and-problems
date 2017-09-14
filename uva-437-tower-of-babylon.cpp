57828600d606794fbbfb69a70fcd48981bd3fd22
Mai Truong <maitruong2801@gmail.com>
2017-09-13 21:25:47 -0400

    Update uva-437-tower-of-babylon.cpp

--- uva-437-tower-of-babylon.cpp
+++ uva-437-tower-of-babylon.cpp
@@ -2,30 +2,47 @@
 using namespace std;
 // global variables
 bool debug = false;
-int n, area = 1000000, block = 1, size[100][3], x, y, z;
+int n;
+struct brick {
+	int x, y, z;
+	block(int a, int b, int c) {
+		x = a; y = b; z = c;
+	}
+	bool operator < (brick b) const {
+		if (x < b.x && y < b.y) return true;
+		else return false;
+	}
+};
+vector<brick> bricks;
 int main() {
-    ifstream cin("babylon.inp");
+    //ifstream cin("babylon.inp");
     ios_base::sync_with_stdio(false); 
     cin.tie(NULL);
     while (cin >> n && n != 0) {
     	for (int i = 1; i <= n; i++) {
+    		int x, y, z;
     		cin >> x >> y >> z;
-    		int size1 = dim[i][1]*dim[i][2], size2= dim[i][1]*dim[i][3], size3 = dim[i][2]*dim[i][3];
-    		size[block][1] = x; size[block][2] = y; size[block++][3] = z;
-    		if (z != y) {size[block][1] = x; size[block][2] = z; size[block++][3] = y; }
-    		if (x != y) {size[block][1] = y; size[block][2] = z; size[block++][3] = x; }
+    		bricks.push_back(x, y, z);
+    		bricks.push_back(y, x, z);
+    		bricks.push_back(x, z, y);
+    		bricks.push_back(z, x, y);
+    		bricks.push_back(y, z, x);
+    		bricks.push_back(z, y, x);
     	}	
-    	// sort(size.begin(), size.end());
-    	int LIS[block+5], maxH = 0;
-    	for (int i = 1; i <= block; i++) {
+    	sort(bricks.begin(), bricks.end());
+    	int num = bricks.size() + 5;
+    	int LIS[num], totalMaxH = 0, maxH = 0;
+    	fill(LIS, LIS + num, 1);
+    	for (int i = 1; i <= num; i++) {
     		for (int j = i-1; j >= 1; j--) {
-    			if (size[j][1] > size[i][1] && size[j][2] > size[i][2] && LIS[j]+1 > LIS[i]) {
+    			if (bricks[j].x > bricks[i].x && bricks[j].y > bricks[i].y && LIS[j]+bricks[i].z > LIS[i]) {
     				LIS[i] = LIS[j] + 1;
-    				size[i][3] += size[j][3];
+    				maxH = max(maxH, bricks[i].z + LIS[j]);
        			}
     		}
-    		maxH = max(maxH, size[i][3]);
+    		bricks[i] = maxH;
+    		totalMaxH = max(totalMaxH, maxH);
     	} 
-    	cout << maxH << "\n";
+    	cout << totaMaxH << "\n";
     }
 }
