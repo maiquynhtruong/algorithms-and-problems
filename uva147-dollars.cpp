332c41f502ce1919e5eb20ef1d8e06fb061d9d98
Mai Truong <maitruong2801@gmail.com>
2017-09-16 19:52:22 -0400

    Update uva147-dollars.cpp

--- uva147-dollars.cpp
+++ uva147-dollars.cpp
@@ -1,29 +1,32 @@
 #include <bits/stdc++.h> 
 using namespace std;
+#define N 30005
 // global variables
-bool debug = true;
+bool debug = false;
 double amount;
-int a, denominations = 11;
-long dp[30005];
+int denominations = 11;
+long long dp[N];
 int c[12] = {0, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000};
 int main() {
     ifstream cin("dollars.inp");
     ios_base::sync_with_stdio(false); 
     cin.tie(NULL);
-    while (cin >> amount && amount > 0) {
-    	a = amount*100;
-        // if(debug) for (int j = 1; j <= denominations; j++) cout << c[j] << ", ";
-    	for (int j = 0; j <= denominations; j++) dp[c[j]] = 1;
-    	for (int i = 1; i <= a; i++) {
-    		for (int j = 1; j <= denominations; j++) {
-    			if (c[j] < i && i%c[j] == 0 && dp[i-c[j]] > 0) {
-                    
-                    dp[i] += dp[i-c[j]];
-                    if (debug) cout << i << " " << c[j] << " " << dp[i-c[j]] << " " << dp[i] << "\n";
-                }
-    		}
-    	}
-        if (debug) cout << "ans: ";
-    	cout << a << " " << dp[a] << "\n";
+    dp[0]=1;
+    for (int j = 1; j <= denominations; j++) {
+        for (int i = c[j]; i <= N; i++) {
+            dp[i] += dp[i-c[j]];
+        }
+        if (debug) {
+            for (int i = 1; i <= N; i++) cout << i << " ";
+            cout << "\n";
+            for (int i = 1; i <= N; i++) cout << dp[i] << " ";
+            cout << "\n";    
+        }
+        
+    }
+    cout << fixed << right << showpoint;
+    while (cin >> amount && amount > 0) { 
+        unsigned int a = (unsigned) ((amount+0.001)*100);
+    	cout << setw(6) << setprecision(2) << amount << setw(17) << dp[a] << "\n";
     }
 }
