871a7139ddbde29c571c73bac0aeb39fa63e6584
Mai Truong <maitruong2801@gmail.com>
2017-09-19 17:38:55 -0400

    Update uva10819-trouble-of-13-dots.cpp

--- uva10819-trouble-of-13-dots.cpp
+++ uva10819-trouble-of-13-dots.cpp
@@ -1,25 +1,32 @@
 #include <bits/stdc++.h> 
 using namespace std;
 bool debug = false;
-int m, n, p[4005], f[4005], v[35], dp[35][1005];
+int m, n, p[105], f[105], dp[105][10205];
 int main() {
-	ifstream cin("diving-for-gold.inp");
+	// ifstream cin("diving-for-gold.inp");
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     while (cin >> m >> n) {
-    	if (m >= 2000) m += 200;
     	for (int i = 1; i <= n; i++) {
     		cin >> p[i] >> f[i];
     	}
-    	for (int i = 1; i <= n) {
-    		for (int money = 0; money <= m; money++) {
-    			if (p[i] <= money) {
-    				dp[i][money] = dp[i-1][money];
-    				if (dp[i-1][money] < dp[i-1][money-p[i]]) {
-    					dp[i][money] = dp[i-1][money-p[i]];
-    				}
-    			}
+    	for (int i = 1; i <= n; i++) {
+    		for (int money = 0; money <= max(2000, m); money++) {
+			dp[i][money] = dp[i-1][money];
+			if (p[i] <= money && dp[i][money] < dp[i-1][money-p[i]] + f[i]) {
+				dp[i][money] = dp[i-1][money-p[i]] + f[i];
+			}
     		}
+	            if (m > 1800) {
+	                for (int money = 2001; money <= m+200; money++) {
+	                    dp[i][money] = dp[i-1][money];
+	                    if (p[i] <= money && dp[i-1][money-p[i]] > 0 && dp[i][money] < dp[i-1][money-p[i]] + f[i]) {
+	                        dp[i][money] = dp[i-1][money-p[i]] + f[i];
+	                    }
+	                }   
+	            }
     	}
+    	if (m > 1800) m+= 200;
+	cout << dp[n][m] << "\n";
     }
 }
