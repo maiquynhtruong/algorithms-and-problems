359f2fa000aecccb4eb47d1fb8dc46588711d8ca
Mai Truong <maitruong2801@gmail.com>
2017-09-19 14:41:13 -0400

    Update uva10664-luggage.cpp

--- uva10664-luggage.cpp
+++ uva10664-luggage.cpp
@@ -3,13 +3,13 @@ using namespace std;
 bool debug = false;
 int m, n;
 int main() {
-	ifstream cin("luggage.inp");
+	//ifstream cin("luggage.inp");
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cin >> m;
     cin.ignore();
     while (m--) {
-    	int dp[25][105], sc[25], n=0;
+    	int dp[25][105][105], sc[25], n=0;
     	int sum=0, s;
     	string line;
     	getline(cin, line);
@@ -20,16 +20,27 @@ int main() {
     		sum += s;
     	}
     	if (debug) cout << "sum= " << sum << "\n";
-    	if (sum % 2 != 0) {
-    		cout << "NO" << "\n"; continue;
+    	if (sum % 2 != 0) {cout << "NO" << "\n"; continue;
     	} else sum /= 2;
-        for (int i = 0; i <= n; i++) fill(dp[i], dp[i]+sum+5, 0);
-    	for (int i = 1; i <= n; i++) {
-    		for (int w = 0; w <= sum; w++) {
-                if (sc[i] <= w) dp[i][w] = dp[i-1][w-sc[i]] || dp[i-1][w];
-    		}
+    	for (int i = 0; i <= n; i++) for (int j = 0; j <= sum+5; j++) fill(dp[i][j], dp[i][j]+sum+5, 0);
+	dp[0][0][0]=1;
+    	for (int i = 0; i <= n; i++) {
+    	     for (int w1 = 0; w1 <= sum; w1++) {
+    	     	for (int w2 = 0; w2 <= sum; w2++) {
+            	     if (sc[i] <= w1) dp[i][w1][w2]= dp[i][w1][w2] || dp[i-1][w1-sc[i]][w2];
+            	     if (sc[i] <= w2) dp[i][w1][w2]= dp[i][w1][w2] || dp[i-1][w1][w2-sc[i]];
+    	     	}
+    	     }
     	} 
-    	if (dp[n][sum] == 1) cout << "YES" << "\n";
+    	if (debug) {
+	         for (int w1 = 0; w1 <= sum; w1++) {
+	         	for (int w2 = 0; w2 <= sum; w2++) {
+		    cout << dp[n][w1][w2] << " ";
+	         	}
+	         	cout << "\n";
+	         }
+    	}
+    	if (dp[n][sum][sum] == 1) cout << "YES" << "\n";
     	else cout << "NO" << "\n";
     }
     return 0;
