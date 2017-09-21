153f7d6ceb7aca0e0fe0f31ce730efdb129dd603
Mai Truong <maitruong2801@gmail.com>
2017-09-21 00:03:17 -0400

    Update uva10819-trouble-of-13-dots.cpp

--- uva10819-trouble-of-13-dots.cpp
+++ uva10819-trouble-of-13-dots.cpp
@@ -1,41 +1,49 @@
 #include <bits/stdc++.h> 
-#define MEM(a,x) memset(a,x,sizeof(a))  
 using namespace std;
-int n;
-bool debug = true;
-string filename = "common_code";
-bool checkRepeating(string s, int len, int i) {
-	int prev = i - len;
-	if (prev < len) return false;
-	for (int p1 = i, int p2 = prev; p1 >= prev+1; p1--, p2--) {
-		if (s[p1] != s[p2]) return false;
-	}
-	return true;
-}
+bool debug = false;
+int m, n, dp[10205][105], p[105], f[105], neg = 20000000;
 int main() {
-    ifstream cin(filename+".inp");
-    // freopen(filename+".out","w",stdout);
+	// ifstream cin("trouble-of-13-dots.inp");
+	// freopen("trouble-of-13-dots.out","w",stdout);
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    string str;
-    while (getline(cin, str) && str[0] != '0') {
-    	if (debug) cout << str << "\n";
-    	stringstream ss(str);
-    	vector<char> vec;
-    	char c;
-    	while (ss >> c) {
-    		vec.push_back(c);
+
+    while (!cin.eof()) {
+    	cin >> m >> n;
+    	int total = 0;
+    	// cout << "Test " << ++test << ":\n" << "m= " << m << " n= " << n << "\n";
+    	m += 200;
+    	if (cin.eof()) break;
+    	for (int i = 1; i <= n; i++) {
+    		cin >> p[i] >> f[i];
+    		total += p[i];
+    		for (int j = 0; j <= m; j++) {
+    			dp[j][i] = -neg;
+    		}
+    		// cout << clothes[i].first << " " << clothes[i].second << "\n";
     	}
-    	n = vec.size();
-    	for (int i = 1; i <= n; i++)
-    		for (int j = 1; j <= n; j++)
-    			dp[i][j] = 1;
-    	for (int len = 1; len < n; len++) { 
-	    	for (int i = 1; i+len <= n; i++) {
-	    		// for (int j = i; j <= i+len; j++) {
-	    		if (checkRepeating(dp, len, i)) dp[len][i] = dp[len][i-len] +1;
-	    		// }
-	    	}
-	    }
+
+    	for (int i = 0; i <= n; i++) {
+    		total += p[i];
+    		for (int j = 0; j <= m; j++) {
+    			dp[j][i] = -neg;
+    		}
+    		// cout << clothes[i].first << " " << clothes[i].second << "\n";
+    	}
+    	
+    	dp[0][0]=0;
+    	
+    	for (int i = 1; i <= n; i++) {
+    		for (int money = 0; money <= min(m, total); money++) {
+				if (p[i] <= money) dp[money][i] = max(dp[money][i-1], dp[money-p[i]][i-1] + f[i]);
+				else dp[money][i] = dp[money][i-1];
+    		}
+    	}
+
+    	m -= 200;
+    	int result = 0;
+    	for (int money = 0; money <= m; money++) result = max(result, dp[money][n]);
+    	for (int money = 2001; money <= m+200; money++) result = max(result, dp[money][n]);
+		cout << result << "\n";
     }
 }
