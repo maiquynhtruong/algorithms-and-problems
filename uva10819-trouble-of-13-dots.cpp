bf12231b0ba4ad5b57cc238b6db5d0b128f1a2ae
Mai Truong <maitruong2801@gmail.com>
2017-09-21 00:02:48 -0400

    Update uva10819-trouble-of-13-dots.cpp

--- uva10819-trouble-of-13-dots.cpp
+++ uva10819-trouble-of-13-dots.cpp
@@ -1,32 +1,41 @@
 #include <bits/stdc++.h> 
+#define MEM(a,x) memset(a,x,sizeof(a))  
 using namespace std;
-bool debug = false;
-int m, n, p[105], f[105], dp[105][10205];
+int n;
+bool debug = true;
+string filename = "common_code";
+bool checkRepeating(string s, int len, int i) {
+	int prev = i - len;
+	if (prev < len) return false;
+	for (int p1 = i, int p2 = prev; p1 >= prev+1; p1--, p2--) {
+		if (s[p1] != s[p2]) return false;
+	}
+	return true;
+}
 int main() {
-	// ifstream cin("diving-for-gold.inp");
+    ifstream cin(filename+".inp");
+    // freopen(filename+".out","w",stdout);
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    while (cin >> m >> n) {
-    	for (int i = 1; i <= n; i++) {
-    		cin >> p[i] >> f[i];
+    string str;
+    while (getline(cin, str) && str[0] != '0') {
+    	if (debug) cout << str << "\n";
+    	stringstream ss(str);
+    	vector<char> vec;
+    	char c;
+    	while (ss >> c) {
+    		vec.push_back(c);
     	}
-    	for (int i = 1; i <= n; i++) {
-    		for (int money = 0; money <= max(2000, m); money++) {
-			dp[i][money] = dp[i-1][money];
-			if (p[i] <= money && dp[i][money] < dp[i-1][money-p[i]] + f[i]) {
-				dp[i][money] = dp[i-1][money-p[i]] + f[i];
-			}
-    		}
-	            if (m > 1800) {
-	                for (int money = 2001; money <= m+200; money++) {
-	                    dp[i][money] = dp[i-1][money];
-	                    if (p[i] <= money && dp[i-1][money-p[i]] > 0 && dp[i][money] < dp[i-1][money-p[i]] + f[i]) {
-	                        dp[i][money] = dp[i-1][money-p[i]] + f[i];
-	                    }
-	                }   
-	            }
-    	}
-    	if (m > 1800) m+= 200;
-	cout << dp[n][m] << "\n";
+    	n = vec.size();
+    	for (int i = 1; i <= n; i++)
+    		for (int j = 1; j <= n; j++)
+    			dp[i][j] = 1;
+    	for (int len = 1; len < n; len++) { 
+	    	for (int i = 1; i+len <= n; i++) {
+	    		// for (int j = i; j <= i+len; j++) {
+	    		if (checkRepeating(dp, len, i)) dp[len][i] = dp[len][i-len] +1;
+	    		// }
+	    	}
+	    }
     }
 }
