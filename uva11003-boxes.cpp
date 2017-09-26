081f56697ec238156c4e48449153f6d3830f2dbc
Mai Truong <maitruong2801@gmail.com>
2017-09-26 16:20:08 -0400

    Update uva11003-boxes.cpp

--- uva11003-boxes.cpp
+++ uva11003-boxes.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h> 
 using namespace std;
 bool debug = false;
-int n, L[1005], W[1005], m = 0, dp[1005][6005];
+int n, L[1005], W[1005], m = 0, dp[6005], inf = 100000;
 int main() {
 	// ifstream cin("easy-arithmetic.inp");
     ios_base::sync_with_stdio(false);
@@ -11,31 +11,19 @@ int main() {
 			cin >> W[i] >> L[i];
 			if (W[i] + L[i] > m) m = W[i] + L[i];
 		}
-		// cout << "m= " << m << "\n";
-		for (int i = 1; i <= n; i++) {
-			for (int j = 0; j <= m; j++) {
-				dp[i][j] = -1000000;
-			}
-		}
-		dp[0][0] = 0;
-		for (int i = 1; i <= n; i++) {
-			for (int j = 0; j <= m; j++) {
-				dp[i][j] = dp[i-1][j];
-				if (W[i] <= j && dp[i-1][j-W[i]] + 1 > dp[i][j]) {
-					dp[i][j] = dp[i-1][j-W[i]] + 1;
-					// cout << i << ", " << j << ", " << dp[i][j] << "\n";
-				}
-			}
-		}
-		// for (int i = 0; i <= n; i++) {
-		// 	for (int j = 0; j <= m; j++) {
-		// 		cout << dp[i][j] << " ";
-		// 	}
-		// 	cout << "\n";
-		// }
-		int res = dp[n][m];
 		for (int j = 0; j <= m; j++) {
-			if (res < dp[n][j]) res = dp[n][j];
+			dp[j] = -1;
+		}
+		dp[0] = inf;
+		for (int i = 1; i <= n; i++) {
+			for (int j = i; j >= 0; j--) {
+				dp[j+1] = max(dp[j+1], min(dp[j]-W[i], L[i]));
+			}
+		}
+		int res = m;
+		for (int j = m; j >= 0; j--) {
+			if (dp[j] == -1) res--;
+			else break;
 		}
 		cout << res << "\n";
 	}   
