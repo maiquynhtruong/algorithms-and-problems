8bc08e24242803ed199ebe5d29bf8e41c7bfd1e8
Mai Truong <maitruong2801@gmail.com>
2017-09-27 14:25:14 -0400

    Update uva11658-best-coalitions.cpp

--- uva11658-best-coalitions.cpp
+++ uva11658-best-coalitions.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h> 
 using namespace std;
 bool debug = true;
-int n, x, p[105], dp[105][10005];
+int n, x, p[105], dp[10005];
 int main() {
 	ifstream cin("best-coalitions.in");
     ios_base::sync_with_stdio(false);
@@ -13,28 +13,21 @@ int main() {
     		p[i] = t * 100;
     	}	
     }
+ 	for (int j = 0; j <= 10005; j++) {
+ 		dp[j] = -10000;
+ 	}
+ 	dp[0] = 1;
  	for (int i = 1; i <= n; i++) {
+ 		if (i == x) continue;
  		for (int j = 0; j <= 10005; j++) {
- 			dp[i][j] = -10000;
+ 			dp[j+p[i]] = dp[j] || dp[j+p[i]];
  		}
  	}
- 	dp[0][0] = 1;
- 	for (int i = 1; i <= n; i++) {
- 		for (int j = 0; j <= 10005; j++) {
- 			
- 			if (dp[i-1][j] >= 0) {
- 				if (j + p[i] > 5000) dp[i][j+p[i]] = max(p[i]/(j+p[i]), dp[i-1][j]);	
- 				else dp[i][j+p[i]] = p[i];
- 			}
- 		}
- 	}
- 	int res = p[x];
+ 	// int res = p[x];
  	for (int j = 50; j <= 10005; j++) {
- 		if (0 < dp[x][j]) {
- 			cout << dp[x][j]/100 << "\n";
+ 		if (0 < dp[j]) {
+ 			cout << p[x]/dp[j] << "\n";
  			break;
  		}
  	}
-
-
 }
