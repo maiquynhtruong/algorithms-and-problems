4c9f32ab2b4e094ee70ebde27099baa6ac8cfb18
Mai Truong <maitruong2801@gmail.com>
2017-10-03 13:44:35 -0400

    Update uva10034-freckles.cpp

--- uva10034-freckles.cpp
+++ uva10034-freckles.cpp
@@ -5,25 +5,33 @@ int n, test, mstCost;
 vector<pair<int, int> > points;
 vector<pair<int, pair<int, int> > > edges;
 int parent[105], rank[105];
+
+void makeSet(int x) {
+	parent[x] = x;
+	rank[x] = 0;
+}
 int find(int x) {
-	if (parent[x] != x) {
-		parent[x] = find(parent[x]);
+	while (x != parent[x]) {
+		x = parent[x];
 	}
-	return parent[x];
+	return x;
 }
 
 void union(int x, int y) {
 	int xRoot = find(x), yRoot = find(y);
-	if (rank[parent[xRoot]] < rank[parent[yRoot]]) {
-		parent[xRoot] = yRoot;
-	} else (rank[parent[xRoot]] > rank[parent[yRoot]]) {
+	if (xRoot == yRoot) return;
+	// Attach smaller rank tree under root of high rank tree (Union by Rank)
+	if (xRoot > yRoot) {
 		parent[yRoot] = xRoot;
 	} else {
-		parent[yRoot] = xRoot;
-		rank[xRoot]++;
-	}
+		parent[xRoot] = yRoot;
+		// If ranks are same, then make one as root and increment its rank by one
+		if (rank[xRoot] == rank[yRoot]) rank[yRoot]++;
+	} 
 }
-
+compare(pair<int, pair<int, int> > a, <pair<int, pair<int, int> > b) {
+	return a.first > b.first;
+}
 int main() {
 //ifstream cin("freckles.in");
     ios_base::sync_with_stdio(false);
@@ -40,15 +48,13 @@ int main() {
     		for (int j = 1; j < points.size(); j++) {
     			int x = points[j].first - u, y = points[j].second - v;
     			edges.push_back(make_pair(x*x + y*y, make_pair(j, i)));
-    			// edges.push_back(make_pair(x*x + y*y, make_pair(i, points[j].first)));
     		}
     		points.push_back(make_pair(i, make_pair(u, v)));
     	}
-    	sort(edges.begin(), edges.end());
+    	sort(edges.begin(), edges.end(), compare);
     	for (int i = 1; i < edges.size(); i++) {
-    		pair<int, pair<int, int> > edge = edges[i];
     		if (find(edges[i].second.first) != find(edges[i].second.second)) {
-    			mstCost += edges[i].first;
+    			mstCost += sqrt(edges[i].first);
     			unionSet(edges[i].second.first, edges[i].second.second);
     		}
     	}
