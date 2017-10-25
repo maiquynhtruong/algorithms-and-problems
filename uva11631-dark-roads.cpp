afc009a93698bd6194b3185c77209df83e8db6f3
Mai Truong <maitruong2801@gmail.com>
2017-10-25 14:28:46 -0400

    Update and rename uva11631-Dark -roads.cpp to uva11631-dark-roads.cpp

--- uva11631-dark-roads.cpp
+++ uva11631-dark-roads.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h> 
 #define LL long long
-#define maxN 400000 
+#define maxN 200000 
 using namespace std;
 bool debug = false;
-int test, n, m, parent[maxN], rank[maxN];
+int test, n, m, parent[maxN+5], r[maxN+5];
 typedef struct {
 	int x, y, z;
 } Edge;
@@ -12,44 +12,43 @@ bool comp(Edge &x, Edge &y) {
 	if (x.z < y.z) return true;
 	return false;
 }
-void initi(int n) {
-	for (int i = 0; i <= n; i++) {
-		parent[i] = i;
-		rank[i] = 1;
-	}
-	edges.clear();
-}
 int findParent(int x) {
 	while (x != parent[x]) x = parent[x];
 	return x;
 }
 void uf_union(int x, int y) {
 	int px = findParent(x), py = findParent(y);
-	if (rank[px] > rank[py]) {
+	if (r[px] > r[py]) {
 		parent[py] = px;
-		rank[px] += rank[py];
+		r[px] += r[py];
 	} else {
 		parent[px] = py;
-		rank[py] + rank[px];
+		r[py] + r[px];
 	}
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int totalCost = 0, mspCost = 0;
-    while (cin >> n >> m) {
-    	initi(m);
-    	for (int i = 1; i <= m; i++) {
+    while (cin >> m >> n) {
+        if (m == 0 && n == 0) break;
+        int totalCost = 0, mspCost = 0, streetCount = m-1;
+        parent[0] = n; r[0]=1;
+    	for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+            r[i] = 1;
     		cin >> edges[i].x >> edges[i].y >> edges[i].z;
-    		totalDis += edges[i].z;
+    		totalCost += edges[i].z;
     	}
-    	sort(edges, edges+m+1, comp);
-    	for (int i = 1; i <= m; i++) {
-    		int px = findParent(edges[i].x), py findParent(edges[i].y]);
+        // parent[n] = n; r[n]=1;
+    	sort(edges+1, edges+n, comp);
+    	// for (int i = 0; i < n && streetCount; i++) {
+        for (int i = 1; i <= n && streetCount; i++) {
+    		int px = findParent(edges[i].x), py = findParent(edges[i].y);
     		if (px != py) {
     			mspCost += edges[i].z;
     			uf_union(px, py);
+                streetCount--;
     		}
     	}
     	cout << totalCost - mspCost << "\n";
